2cc4ff34900d7ab7983ca23fef04e22cd6dcd6b2
Shazidulislam <shazidulislam910@gmail.com>
2026-06-27 23:55:12 +0600

    lerarn file system in node.js

--- index.js
+++ index.js
@@ -2,24 +2,56 @@ const fs = require('node:fs');
 
 // fs.writeFile(file, data[, options], callback)
 
-fs.writeFile("File.txt" , "Hey hello data" , function(err){
+// fs.writeFile("File.txt" , "Hey hello data" , function(err){
 
-    if(err)console.log(err);
-    else console.log("Everything is okey here!")
-})
+//     if(err)console.log(err);
+//     else console.log("Everything is okey here!")
+// })
 
 // fs.appendFile(path, data[, options], callback)
 
-fs.appendFile("File.txt" , " Kyse ho app" ,function(err){
-    if(err)console.log(err)
-       else console.log("Successfully run appendFile ")
-})
+// fs.appendFile("File.txt" , " Kyse ho app" ,function(err){
+//     if(err)console.log(err)
+//        else console.log("Successfully run appendFile ")
+// })
 
 // fs.rename(oldPath, newPath, callback)
 
-fs.rename("File.txt" , "Message.tst" , function(err){
-    if(err)console.log(err)
-       else console.log("Rname Complete!")
-})
+// fs.rename("File.txt" , "Message.tst" , function(err){
+//     if(err)console.log(err)
+//        else console.log("Rname Complete!")
+// })
 
 // fs.copyFile(src, dest[, mode], callback)
+
+// fs.copyFile("File.txt" ,"./copeey/destination.txt" , function(err){
+//     if(err) throw err;
+//     else console.log("File.txt was copied to destination.txt")
+// } )
+
+// fs.unlink(path, callback)
+//  removes a file or symbolic link
+
+// fs.unlink("Message.ts" , function(err){
+//     if(err) throw err
+//     console.log("path/Message.ts file was deleted ")
+// })
+
+// fs.rmdir(path[, options], callback)
+// fs.rmdir() is used to remove (delete) a directory (folder) in Node.js.
+
+
+// fs.rmdir("./copeey",{recursive:true} ,function(err){
+//     if(err) console.log(err)
+//     else console.log("Removed successfully")
+// })
+
+
+
+// !fs.rm(path[, options], callback)
+// fs.rm() is a Node.js method used to delete files or folders.
+
+// fs.rm("./copeey",{recursive:true} ,function(err){
+//     if(err) console.log(err)
+//     else console.log("Removed successfully")
+// })
